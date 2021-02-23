26bcb68d69e7a48a7bc8d329ab60c575c8a71d78
S1du5 <sidus.master@gmail.com>
2021-02-22 22:38:06 -0300

    Adição de Grid ao Layout do app

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { Button } from '../src/components/commons/Button'
 import Menu from '../src/components/commons/Menu'
 import Footer from '../src/components/commons/Footer' 
 import Text from '../src/components/foundations/Text'
+import { Grid } from '../src/components/layout/Grid'
 
 const Home = () => {
   return (
@@ -14,41 +15,59 @@ const Home = () => {
     }}>
       <Menu />
 
-      <div>
-        <Text 
-          variant="title"
-          tag="h1"
-          color="tertiary.main"
-          textAlign={{
-            xs: 'center',
-            sm: 'left'
-          }}
-        >
-          Compartilhe momentos e conecte-se com amigos
-        </Text>
-        <Text 
-          variant="paragraph1"
-          tag="p"
-          color="tertiary.light"
-          textAlign={{
-            xs: 'center',
-            sm: 'left'
-          }}
-        >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-        </Text>
-        
-        <Button 
-          variant="primary.main"
-          display="block"
-          margin={{
-            xs: 'auto',
-            sm: 'initial'
-          }}
-        >
-          Cadastrar
-        </Button>
-      </div>
+      <Grid.Container>
+        <Grid.Row>
+          <Grid.Col
+            offset={{ xs: 0, md: 1 }}
+            value={{ xs: 12, md: 5 }}
+          >
+            <div>
+              <Text
+                variant="title"
+                tag="h1"
+                color="tertiary.main"
+                textAlign={{
+                  xs: 'center',
+                  md: 'left',
+                }}
+              >
+                Compartilhe momentos e conecte-se com amigos
+              </Text>
+              <Text
+                variant="paragraph1"
+                tag="p"
+                color="tertiary.light"
+                textAlign={{
+                  xs: 'center',
+                  md: 'left',
+                }}
+              >
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.
+              </Text>
+
+              <Button
+                variant="primary.main"
+                margin={{
+                  xs: 'auto',
+                  md: 'initial',
+                }}
+                display="block"
+              >
+                Cadastrar
+              </Button>
+            </div>
+          </Grid.Col>
+          <Grid.Col
+            value={{ xs: 12, md: 6 }}
+          >
+            <img
+              style={{ display: 'block', margin: 'auto' }}
+              src="https://bootcamp-alura-01-git-modulo01.omariosouto.vercel.app/images/phones.png"
+            />
+          </Grid.Col>
+        </Grid.Row>
+      </Grid.Container>
 
       <Footer />
     </div>
