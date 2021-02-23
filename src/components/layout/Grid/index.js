e37d783f901bb300cb4bf2d1e230fd2e87c1d81e
S1du5 <sidus.master@gmail.com>
2021-02-22 22:52:04 -0300

    Mudança

--- src/components/layout/Grid/index.js
+++ src/components/layout/Grid/index.js
@@ -2,6 +2,36 @@ import styled, { css } from 'styled-components';
 import { breakpointsMedia } from '../../../theme/utils/breakpointsMedia';
 // import { propToStyle } from '../../../../theme/utils/propToStyle';
 
+const Container = styled.div`
+  width: 100%;
+  padding-right: 28px;
+  padding-left: 28px;
+  margin-right: auto;
+  margin-left: auto;
+  ${breakpointsMedia({
+  xs: css`
+    max-width: initial;
+
+    padding-right: 28px;
+    padding-left: 28px;
+  `,
+  sm: css`
+    max-width: 576px; 
+  `,
+  md: css`
+    max-width: 768px;
+    padding-right: 16px;
+    padding-left: 16px; 
+  `,
+  lg: css`
+    max-width: 1160px; 
+  `,
+  xl: css`
+    max-width: 1222px;
+  `,
+})}
+`
+
 const Col = styled.div`
   padding-right: 16px;
   padding-left: 16px;
@@ -82,7 +112,7 @@ const Col = styled.div`
       }),
     });
   }}
-`;
+`
 
 Col.defaultProps = {
   value: {},
@@ -90,35 +120,7 @@ Col.defaultProps = {
 };
 
 export const Grid = {
-  Container: styled.div`
-    width: 100%;
-    padding-right: 28px;
-    padding-left: 28px;
-    margin-right: auto;
-    margin-left: auto;
-    ${breakpointsMedia({
-    xs: css`
-      max-width: initial;
- 
-      padding-right: 28px;
-      padding-left: 28px;
-    `,
-    sm: css`
-      max-width: 576px; 
-    `,
-    md: css`
-      max-width: 768px;
-      padding-right: 16px;
-      padding-left: 16px; 
-    `,
-    lg: css`
-      max-width: 1160px; 
-    `,
-    xl: css`
-      max-width: 1222px;
-    `,
-  })}
-  `,
+  Container,
   Row: styled.div`
     display: flex;
     flex-wrap: wrap;
